ed51708a1f120d726cff5bfdbfc7093687921357
Niharika <niharika.3297@gmail.com>
2020-04-17 23:39:08 +0530

    Send transaction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate web3;
 
 use web3::futures::Future;
+use web3::types::{TransactionRequest, U256};
 
 fn main() {
     let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
@@ -8,7 +9,18 @@ fn main() {
     let web3 = web3::Web3::new(transport);
     let accounts = web3.eth().accounts().wait().unwrap();
 
-    let balance = web3.eth().balance(accounts[1], None).wait().unwrap();
+    let tx = TransactionRequest {
+        from: accounts[0],
+        to: Some(accounts[1]),
+        gas: None,
+        gas_price: None,
+        value: Some(U256::from(10000)),
+        data: None,
+        nonce: None,
+        condition: None
+    };
 
-    println!("Balance: {}", balance);
+    let tx_hash = web3.eth().send_transaction(tx).wait().unwrap();
+
+    println!("TX Hash: {:?}", tx_hash);
 }
